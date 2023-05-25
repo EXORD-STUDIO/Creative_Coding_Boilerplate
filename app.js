2d85fc96443e5bbef1f94801c4470150bb9c2205
Fokarr <jarmof135@gmail.com>
2023-05-26 01:12:38 +0200

    port change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import path from "path";
 import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
